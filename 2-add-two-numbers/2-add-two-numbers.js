56467295b2c0abd7b7bfcfd89770f4e1dd72eb10
I am ne zha / Jeskson <59645426+webVueBlog@users.noreply.github.com>
2022-06-23 01:16:25 +0800

    Time: 140 ms (60.86%), Space: 47.7 MB (32.44%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.js
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -11,19 +11,21 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let res = new ListNode();
-    let carry = 0;
-    let node = res;
-    while(l1 || l2 || carry > 0) {
-        let v1 = l1 ? l1.val : 0,
-            v2 = l2 ? l2.val : 0
-        let sum = v1 + v2 + carry
-        carry = Math.floor(sum/10)
-        sum = sum % 10
-        node.next = new ListNode(sum)
-        if(l1) l1 = l1.next
-        if(l2) l2 = l2.next
-        node = node.next
+    const dummy = new ListNode();
+    let [curr, carry] = [dummy, 0];
+    
+    while(l1 || l2) {
+        let sum = 0;
+        sum += (l1?.val || 0) + (l2?.val || 0) + carry;
+        curr.next = new ListNode(sum % 10);
+        carry = Math.floor(sum / 10);
+        
+        l1 = l1?.next;
+        l2 = l2?.next;
+        curr = curr.next;
     }
-    return res.next;
+        
+    if (carry) curr.next = new ListNode(carry);
+    
+    return dummy.next;
 };
