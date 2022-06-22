8762170477791194a04193b103abc86045680bfd
I am ne zha / Jeskson <59645426+webVueBlog@users.noreply.github.com>
2022-06-23 02:07:09 +0800

    Time: 126 ms (64.79%), Space: 47 MB (69.46%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -3,17 +3,14 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(s === '') return 0
-    let map = new Map()
-    let startIdx = 0
-    let max = 0
-    for(let i = 0; i < s.length; i++) {
-        let cur = s[i]
-        if(map.has(cur) && map.get(cur) >= startIdx) {
-            startIdx = map.get(cur) + 1
-        }
-        map.set(cur, i)
-        max = Math.max(max, i-startIdx+1)
+    const set = new Set();
+    let [start, end, maxLen] = [0, 0, 0];
+    
+    while(end < s.length) {
+        set.has(s[end]) ? set.delete(s[start++]) : set.add(s[end++]);
+        
+        maxLen = Math.max(maxLen, set.size);
     }
-    return max
+    
+    return maxLen;
 };
