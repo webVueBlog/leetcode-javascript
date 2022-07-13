b7769ba29778f43221caee89c21b0f9841cd7158
I am ne zha / Jeskson <59645426+webVueBlog@users.noreply.github.com>
2022-07-13 12:16:31 +0800

    Time: 234 ms (86.14%), Space: 50.8 MB (43.07%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function(s, words) {
+    const [wordsCount, size] = [words.length, words[0].length];
+    const limit = s.length - wordsCount * size;
+    const map = {};
+    const ans = [];
+    
+    words.forEach(word => map[word] ? map[word]++ : map[word] = 1);
+    
+    for (let i = 0; i <= limit; i++) {
+        if (!map[s.slice(i, i + size)]) continue;
+        
+        const seen = {};
+        let [j, count] = [i, 0];
+        while (count < wordsCount) {
+            const sub = s.slice(j, j + size);
+            seen[sub] ? seen[sub]++ : seen[sub] = 1;
+            if (!map[sub] || seen[sub] > map[sub]) break;
+            
+            j += size;
+            count++;
+        }
+        
+        if (count === wordsCount) ans.push(i);
+    }
+    
+    return ans;
+};
