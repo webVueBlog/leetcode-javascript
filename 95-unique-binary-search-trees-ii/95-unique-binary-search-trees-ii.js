adcb7fe0c67cf83b10c9f79a3f475bfc57d54a0a
I am ne zha / Jeskson <59645426+webVueBlog@users.noreply.github.com>
2022-07-14 19:54:20 +0800

    Time: 118 ms (65.32%), Space: 48.4 MB (45.38%) - LeetHub

---
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function(n) {
+    return !n ? [] : helper(1, n);
+};
+
+const helper = (start, end) => {
+    if (start > end) return [null];
+    
+    const ans = [];
+    for (let i = start; i <= end; i++) {
+        const left = helper(start, i - 1);
+        const right = helper(i + 1, end);
+        
+        for (let l of left) {
+            for (let r of right) {
+                const node = new TreeNode(i, l, r);
+                ans.push(node);
+            }
+        }
+    }
+    
+    return ans;
+}
